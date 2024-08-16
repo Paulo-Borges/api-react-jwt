eb6ea898fc97baf3508bf767979f6eb6a4d22b17
Paulo Borges <borgespaulo72@yahoo.com.br>
2024-08-16 10:18:52 -0300

    feat: commit inicial

---
+++ src/pages/Cadastro/index.jsx
@@ -0,0 +1,15 @@
+function Cadastro(){
+    return (
+        <div>
+            <h2>Cadastro</h2>
+            <form action="">
+                <input type="text" placeholder="Nome" />
+                <input type="email" placeholder="Email" />
+                <input type="password" placeholder="Senha" />
+                <button>Cadastrar-se</button>
+            </form>
+        </div>
+    )
+}
+
+export default Cadastro
