ef8d9796e9da8c83b45772f6bd2e91693d6e4b70
Paulo Borges <borgespaulo72@yahoo.com.br>
2024-08-16 17:04:27 -0300

    feat: adicionando caminho para o meu repositório GIT

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,10 +1,12 @@
+import { Link } from 'react-router-dom'
 import React from 'react'
 
 const Footer = () => {
   return (
     
-    <footer className='bg-blue-600 text-white p-4 shadow-md mt-6'>
-      <h1 className='text-2xl font-semibold text-center' >&copy; Borges 2024</h1>
+    <footer className='bg-blue-600 text-white p-4 shadow-md mt-6 bottom-0 w-full fixed text-center'>
+      <Link to={'https://github.com/Paulo-Borges?tab=repositories'} target="_blank" className='text-2xl font-semibold'>&copy; Borges 2024</Link>
+
     </footer>
     
   )
--- src/pages/Lista/index.jsx
+++ src/pages/Lista/index.jsx
@@ -22,7 +22,7 @@ const [allusers, setAllUsers] = useState()
 
 
     return(
-        <div className="max-w-2xl mx-auto mt-10 bg-white p-8 border border-gray-300 rounded-md shadow-lg">
+        <div className="max-w-2xl mx-auto mt-10 bg-white p-8 border border-gray-300 rounded-md shadow-lg mb-20">
             <h2 className="text-2xl font-bold mb-6 text-center text-gray-800">Listar Usuários</h2>
             <ul className="space-y-2">
                 {allusers && allusers.length > 0 && allusers.map( (user) => (
