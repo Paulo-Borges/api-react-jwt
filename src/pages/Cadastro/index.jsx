78f4d95b7596fa0298c43056a8e7ff35390d27bc
Paulo Borges <borgespaulo72@yahoo.com.br>
2024-08-16 11:13:57 -0300

    chore: form finalizado

--- src/pages/Cadastro/index.jsx
+++ src/pages/Cadastro/index.jsx
@@ -1,14 +1,20 @@
+import { Link } from "react-router-dom"
+
 function Cadastro(){
     return (
-        <div>
-            <h2>Cadastro</h2>
-            <form action="">
-                <input type="text" placeholder="Nome" />
-                <input type="email" placeholder="Email" />
-                <input type="password" placeholder="Senha" />
-                <button>Cadastrar-se</button>
+        <div className=" justify-center items-center h-screen flex">
+            <div className="max-w-lg w-full bg-white p-8 border border-gray-300 rounded-lg shadow-lg">
+            <h2 className=" text-2xl font-bold mb-6 text-center text-gray-800">Cadastro</h2>
+            <form action="" className=" flex flex-col gap-5">
+                <input type="text" placeholder="Nome" className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none"/>
+                <input type="email" placeholder="Email"className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none"/>
+                <input type="password" placeholder="Senha" className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none"/>
+                <button className="w-full bg-blue-500 text-white py-2px-4 rounded-md hover:bg-blue-800">Cadastrar-se</button>
             </form>
+            <Link to="/Login" className="text-blue-700 hover:underline mt-4 block text-center">Já tem uma conta? Faça login</Link>
         </div>
+        </div>
+        
     )
 }
 
--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,7 +1,10 @@
+import { Link } from "react-router-dom"
+
 function Login() {
     return (
         <div>
             <h2>Login</h2>
+            <Link to="/" >Cadastro</Link>
         </div>
     )
 }
